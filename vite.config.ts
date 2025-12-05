5505ce8eb98b4c39b0744b378fd1afa48702a413
remi puig <rpuigsech@gmail.com>
2025-12-05 16:25:53 +0100

    changement et mise en prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/OleWebsite/', // ⚠️ Mets le nom exact du repo
+  base: '/', // ⚠️ Mets le nom exact du repo
 
 })
